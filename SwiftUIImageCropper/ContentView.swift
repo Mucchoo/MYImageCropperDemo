b35b1e4f9c27c0fe9fbd00169aca18a0059dace9
Musa <yazujumusa@gmail.com>
2024-12-22 20:33:38 +0900

    Initial Commit

---
+++ SwiftUIImageCropper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIImageCropper
+//
+//  Created by Musa Yazici on 12/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIImageCropper/SwiftUIImageCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImageCropperApp.swift
+//  SwiftUIImageCropper
+//
+//  Created by Musa Yazici on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImageCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
